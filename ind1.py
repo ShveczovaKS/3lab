18250f5898d37990045670ae2ea605ab647fdb11
ShveczovaKS <shveczova_02@bk.ru>
2021-03-25 20:14:25 +0300

    Индивиуальное 1.

---
+++ ind1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+if __name__ == '__main__':
+    n = int(input("n = "))
+    if n > 10:
+        print("Ошибка", file=sys.stderr)
+        exit(1)
+    if n == 1:
+        b = " карандаш"
+    elif n <= 4:
+        b = " карандаша"
+    else:
+        b = " карандашей"
+    print("Я купи7"
+          "ла ", n, b)
