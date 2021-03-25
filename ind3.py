49c57da8a21413f6e9c29c533ac71b82fbdd3826
ShveczovaKS <shveczova_02@bk.ru>
2021-03-25 21:38:02 +0300

    Индивиуальное 3.

---
+++ ind3.py
@@ -0,0 +1,10 @@
+grants = int(input("ежемесячная стипендия"))
+expenses = int(input("ежемесячные расходы"))
+months = 10
+money = 0
+ask_for_money = 0
+while ask_for_money <= months-1:
+    money += expenses - grants
+    expenses *= 1.03
+    ask_for_money += 1
+print("{0:.2f}".format(money))
