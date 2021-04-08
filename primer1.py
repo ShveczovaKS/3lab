e3c8d479aca0205606da2c35a1a980a216085368
ShveczovaKS <shveczova_02@bk.ru>
2021-04-08 23:51:48 +0300

    Пример 1

--- primer1.py
+++ primer1.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import math
+
 if __name__ == '__main__':
     x = float(input("Value of x? "))
     if x <= 0:
@@ -7,4 +10,4 @@ if __name__ == '__main__':
         y = x + 1
     else:
         y = math.sin(x) - x * x
-    print (f"y = {y}" )
+    print(f"y = {y}")
